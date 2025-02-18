e2bed4061ac4f69107dc20c9dde85dcdc3677739
Davis <155711055+davisjabasing@users.noreply.github.com>
2025-02-18 14:21:12 +0530

    Update main.tf

--- blueprint/genesys-cloud-cx-as-code/modules/data_actions/main.tf
+++ blueprint/genesys-cloud-cx-as-code/modules/data_actions/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     genesyscloud = {
       source = "mypurecloud/genesyscloud"
+ 
     }
   }
 }
-
-
+ 
 ###
 #
 # Description:  
@@ -15,30 +15,23 @@ terraform {
 # Lambda that is performing our email-classification.
 #
 ###
-
-
+ 
 resource "genesyscloud_integration" "ComprehendDataAction" {
   intended_state   = "ENABLED"
   integration_type = "custom-rest-actions"
-
   config {
     name       = "ComprehendDataAction"
     properties = jsonencode({})
     advanced   = jsonencode({})
     notes      = "Used to invoke an AWS Comprehend job"
   }
-
-  lifecycle {
-    ignore_changes = [config]
-  }
 }
-
+ 
 resource "genesyscloud_integration_action" "LookupQueueName" {
   name           = "LookupQueueName"
   category       = "ComprehendDataAction"
   integration_id = genesyscloud_integration.ComprehendDataAction.id
   secure         = false
-
   contract_input = jsonencode({
     "type"     = "object",
     "required" = ["EmailSubject", "EmailBody"],
@@ -51,17 +44,17 @@ resource "genesyscloud_integration_action" "LookupQueueName" {
       }
     }
   })
-
   contract_output = jsonencode({
     "type" = "object",
-    "required" = ["QueueName"],
+    "required" = [
+      "QueueName"
+    ],
     "properties" = {
       "QueueName" = {
         "type" = "string"
       }
     }
   })
-
   config_request {
     request_url_template = var.classifier_url
     request_type         = "POST"
@@ -71,14 +64,9 @@ resource "genesyscloud_integration_action" "LookupQueueName" {
       X-API-Key                          = var.classifier_api_key
     }
   }
-
   config_response {
     translation_map          = {}
     translation_map_defaults = {}
     success_template         = "$${rawResult}"
-  }
-
-  lifecycle {
-    ignore_changes = [config_request, config_response]
   }
 }
